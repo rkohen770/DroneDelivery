60866ae25a4c2ce0d9ee20b0006c7e7797688fad
devora zarbiv <dzarbiv@g.jct.ac.il>
2021-10-14 01:17:34 +0300

    initial code+welcome function

---
+++ targil0/Program7418.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome7418();
+            welcome2156();
+            Console.ReadKey();
+        }
+
+        private static void welcome7418()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void welcome2156();
+    }
+}
