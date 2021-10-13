cee37a1a5dd067313f141a7ea6c07fb00f27a378
rachel lea kohen <rkohen@g.jct.ac.il>
2021-10-14 01:31:26 +0300

    my welcome 2156

---
+++ targil0/Program2156.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static partial void welcome2156()
+        {
+            Console.WriteLine("Moshiach Now"); 
+        }
+    }
+}
