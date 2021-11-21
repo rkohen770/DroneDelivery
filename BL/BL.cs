b4567c50751a501ffa4bf925c4c86a0d02d9c86f
rachel lea kohen <rkohen@g.jct.ac.il>
2021-11-21 21:08:18 +0200

    merge concule & IBL

--- BL/BL.cs
+++ BL/BL.cs
@@ -6,16 +6,45 @@ using System.Threading.Tasks;
 using IBL.BO;
 using DalObject;
 using IDAL;
+using IDAL.DO;
 namespace BL
 {
-    public partial class BL :IBL.IBL
+    public partial class BlObject : IBL.IBL
     {
-        IDal dAL = new DalObject.DalObject();
-
+        IDal dal = new DalObject.DalObject();
         //public List<DroneForList> droneForLists=new List<DroneForList>();
-        public BL()
+        public BlObject()
         {
-            
+
+        }
+
+        public void AddBaseStation(int id, string nameBaseStation, Location location, int numOfAvailableChargingPositions)
+        {
+            BaseStation baseStation = new BaseStation();
+            dal.AddStation(id, nameBaseStation, location.Longitude, location.Latitude, numOfAvailableChargingPositions);
+            baseStation.Id = id;
+            baseStation.NameBaseStation = nameBaseStation;
+            baseStation.Location = location;
+            baseStation.NumOfAvailableChargingPositions = numOfAvailableChargingPositions;
+        }
+
+
+        public void AddDrone(int droneId, string model, IBL.BO.WeightCategories maxWeight, int stationId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void AddCustomer(int id, string name, string phone, double longitude, double lattitude)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public int AddParcel(int senderId, int targetId, WeightCategories weight, Enums.Priorities priority, int droneId = 0)
+        {
+            throw new NotImplementedException();
         }
     }
 }
