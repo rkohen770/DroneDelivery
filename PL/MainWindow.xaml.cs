3b10688d228d5b14747ae4e09408de6455cdb6d2
rachel lea kohen <rkohen@g.jct.ac.il>
2021-12-23 17:07:55 +0200

    main window

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BO;
 using BLApi;
+using System.Collections.ObjectModel;
 
 namespace PL
 {
@@ -22,21 +23,51 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// collection of lines
+        /// </summary>
+        ObservableCollection<DroneForList> listDrones;
+
         private IBL bl = BLFactory.GetBL();
         public MainWindow()
         {
             InitializeComponent();
+            //reset the lines list
+            listDrones = new ObservableCollection<DroneForList>(bl.GetAllDronesBo());
+
+
+            //show the lines
+            cmbDronesID.ItemsSource = listDrones;
+            cmbDronesID.DisplayMemberPath = "DroneId";
         }
 
         /// <summary>
-        /// A button that opens a window of the drone details
+        /// A button that opens a window for adding a drone
         /// </summary>
         /// <param name="sender">Button type</param>
         /// <param name="e"></param>
-        private void ShowDroneButton_Click(object sender, RoutedEventArgs e)
+        private void AddDroneButton_Click(object sender, RoutedEventArgs e)
         {
-            new DroneListWindow(bl).Show();
+            new AddDroneWindow(bl, this).ShowDialog();
         }
+
+
+
+
+
+
+
+
+
+
+        ///// <summary>
+        ///// A button that opens a window of the drone details
+        ///// </summary>
+        ///// <param name="sender">Button type</param>
+        ///// <param name="e"></param>
+        //private void ShowDroneButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    new DroneListWindow(bl).Show();
+        //}
 
     }
 }
