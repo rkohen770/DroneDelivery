dae1b95fbd5990d1d9f238f5b560eac538be1fda
rachel lea kohen <rkohen@g.jct.ac.il>
2021-12-26 13:40:10 +0200

    drones

--- BL/Drone.cs
+++ BL/Drone.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BO
 {
-    public class Drone
+    public class Drone: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public int DroneId { get; set; }
         public string DroneModel { get; set; }
         public WeightCategories Weight { get; set; }
-        public double DroneBattery { get; set; }
+        public double DroneBattery {
+            get => DroneBattery;
+            set
+            {
+                DroneBattery = value;
+                OnPropertyChanged(nameof(DroneBattery));
+            }
+        }
         public DroneStatus? DroneStatus { get; set; }
         public ParcelInTransfer ParcelInTransfer { get; set; }
         public Location CurrentLocation { get; set; }
---
+++ PL/Converters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace PL
+{
+    public class DroneID : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value;
+
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value;
+
+        }
+    }
+    public class DroneStatusToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((BO.DroneStatus)value)
+            {
+                case BO.DroneStatus.Available:
+                    return "/images/Check Circle_24px.png";
+                case BO.DroneStatus.Maintenance:
+                    return "/images/maintenance_24px.png";
+
+                case BO.DroneStatus.Delivery:
+                    return "/images/home_24px.png";
+
+            }
+            return Enum.GetName(typeof(BO.DroneStatus), value) + ".png";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
